44bb50db513010409d574ab1eeca6c18b56cfaf9
abdallahelbaggari <abdallahelbaggari@gmail.com>
2026-05-09 18:34:24 +0100

    Add leaderboard data for players

---
+++ 📁 data/leaderboard.js
@@ -0,0 +1,20 @@
+export const leaderboardData = [
+  {
+    username: "Abdullah",
+    points: 120,
+    wins: 10,
+    losses: 2
+  },
+  {
+    username: "Player2",
+    points: 95,
+    wins: 8,
+    losses: 4
+  },
+  {
+    username: "Player3",
+    points: 80,
+    wins: 6,
+    losses: 5
+  }
+];
