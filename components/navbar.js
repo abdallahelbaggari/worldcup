c12ebcadd0f5e653162478ebfbbdf531d5d8788e
abdallahelbaggari <abdallahelbaggari@gmail.com>
2026-05-09 17:46:41 +0100

    Add Navbar component for navigation

---
+++ components/navbar.js
@@ -0,0 +1,18 @@
+export function Navbar() {
+  return `
+    <nav class="navbar">
+
+      <div class="logo">
+        ⚽ WorldCup
+      </div>
+
+      <ul class="nav-links">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Live Matches</a></li>
+        <li><a href="#">Predictions</a></li>
+        <li><a href="#">Leaderboard</a></li>
+      </ul>
+
+    </nav>
+  `;
+}
